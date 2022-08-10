96791e6bcf0d46528133cf44d488f56e9c9bfb4e
Surajit Karuri <surajitkaruri@gmail.com>
2022-08-10 11:02:00 +0530

    template driven

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-todo';
+  title = 'simple TODO app';
+  todos: any = [];
+
+  todo: String = '';
+
+  addTodo(todo: String) {
+    this.todos.unshift(todo);
+  }
+
+  // with formControl
+  addTodoWithKey() {
+    this.todos.unshift(this.todo);
+  }
+
+  deleteTodo(todoIndex: number) {
+    this.todos.splice(todoIndex, 1);
+  }
+
+  todoReset() {
+    this.todo = '';
+  }
 }
